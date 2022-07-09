dfa4b75dcd0df0395f786848bac48142257c4e4f
박상훈 <psh995666@gmail.com>
2022-07-09 22:38:17 +0900

    DIP, OCP 위반

--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -9,18 +9,22 @@ import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService {
 
-    // 주문 생성 요청이 오면, 회원 정보를 요청 > 할인정책 > 최종 할인된 가격을 받아서 > 최종 생성된 주문을 반환
+    // 주문 생성 요청이 들어오면, 회원정보를 요청 > 할인정책 > 최종 할인된 가격을 받아서 > 최종 생성된 주문을 반환
 
-    // MemberRepository에서 회원을 찾아야 하기 때문에 필요
+    // MeberRepository 에서 회원을 찾아야 하기 때문에 선언
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    /* 1) OrderServiceImpl이 DiscountPolicy 인터페이스와 FixDiscountPolicy 인 구현체 클래스도 함꼐 의존하고 있다 = DIP위반
-    *  2) 고정할인(Fix) 정책에서 유동적 할인(Rate) 정책으로 변경
-    *     private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
-    *     그러나 발생하는 문제점은 RateDiscountPolicy로 바꾸는 순간 OrderServiceImpl 에 코드를 수정해야함 이것이 의존성 = OCP위반
-    *     private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
-    *  3) 인터페이스에만 의존하도록 의존관계를 변경
-    *     - OrderServiceImpl을 DiscountPolicy에만 의존하도록코드를 변경
-    *     - final 키워드는 제거 : 값이 무조건 할당되야만 해당 키워드를 선언 가능 */
+
+    /*
+    * 1) OrderServiceImpl 에서 DiscountPolicy 인터페이스와 FixDiscountPolicy 인 구현체 클래스도 함께 의존하고 있다 = DIP 위반
+    * 2) 고정할인(Fix) 정책에서 유동적 할인(Rate) 정책으로 변경
+    *    - private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    *    그러나 발생하는 문제점: RateDiscountPolicy 로 바꾸는 순간 OrderServiceImpl 에 코드를 수정해야 함 이것이 의존성 = OCP 위반
+    *    - private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+    *    OCP 위반: 내가 기름차에서 전기차로 바꿔도 면허증만 있으면 운전이 가능한 것과 동일한 원리
+    * 3) 구현체가 아닌 인터페이스에만 의존하도록 의존관계를 변경
+    *    - OrderServiceImpl 을 DiscountPolicy 에만 의존하도록 코드를 변경
+    *    - final 키워드는 제거 : 값이 무조건 할당되야만 해당 키워드를 선언 가능
+    * */
     private DiscountPolicy discountPolicy;
 
     @Override
