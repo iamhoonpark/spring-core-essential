4cabdfdc7eae7681d4d95dd4cc12c331e1a3daac
박상훈 <psh995666@gmail.com>
2022-07-28 19:54:50 +0900

    Java 코드 -> Spring 으로 전환

--- core/src/main/java/hello/core/MemberApp.java
+++ core/src/main/java/hello/core/MemberApp.java
@@ -4,15 +4,21 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
 
     // 회원 도메인 실행과 테스트
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        // MemberService memberService = new MemberServiceImpl();
-        // AppConfig 에서 memberService 를 달라고하면, memberImpl 객체를 생성하면서 MemoryRepo 를 주입해줌
-        MemberService memberService = appConfig.memberService();
+        // Spring 은 ApplicationContext 에서 시작 = 스프링 컨텍스트 = @Bean 들을 다 관리해줌
+        // 파라미터에 담은 내용들을 Spring 컨텍스트에 객체를 등록하여 관리 시작
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        // 기존 찾아온 방법: AppConfig appConfig = new AppConfig();
+        // 스프링 전환 후 객체를 찾아오는 방법(key, type)
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        // 실행 시, Creating shared instance of singleton bean 'appConfig.~' 스프링에 등록된 것
 
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
--- core/src/main/java/hello/core/OrderApp.java
+++ core/src/main/java/hello/core/OrderApp.java
@@ -7,26 +7,37 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.order.Order;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
 
     public static void main(String[] args) {
 
-        AppConfig appConfig = new AppConfig();
-
         /*MemberService memberService = new MemberServiceImpl();
         OrderService orderService = new OrderServiceImpl();*/
 
+        /* Java Code
+        AppConfig appConfig = new AppConfig();
         MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();
+        OrderService orderService = appConfig.orderService(); */
+
+        // 위의 Java 코드를 Spring 으로 전환
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        OrderService orderService = ac.getBean("orderService", OrderService.class);
+        // 실행 시 Creating shared instance of singleton bean 'appConfig' 스프링으로 객체(빈) 등록
 
         // 신규멤버
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
+
         // 현재 DB가 없으니 메모리 객체에 저장
         memberService.join(member);
+
         // 주문
         Order order = orderService.createOrder(memberId, "Phone Case", 20000);
+
         // 호출
         System.out.println("order = " + order);
         System.out.println("order.calculatePrice = " + order.calculatePrice());
